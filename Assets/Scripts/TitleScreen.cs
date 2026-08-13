68835780d2b775c23c9d4d8ba6dbdb6de54b8309
OnixFang <twily.dev@outlook.com>
2026-08-13 16:29:28 -0400

    feat: make title screen listen to navigation for default selected option

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,12 +1,49 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class TitleScreen : MonoBehaviour
 {
   [SerializeField] GameObject tournamentSettingsPanel;
+  [SerializeField] InputActionReference navigateAction;
+  [SerializeField] GameObject firstMenuObject;
 
   void Start()
   {
     Canvas.ForceUpdateCanvases();
+  }
+
+  void OnEnable()
+  {
+    StartCoroutine(ReselectFirstMenuObject());
+    navigateAction.action.performed += OnNavigate;
+  }
+
+  void OnDisable()
+  {
+    navigateAction.action.performed -= OnNavigate;
+  }
+
+  void OnNavigate(InputAction.CallbackContext context)
+  {
+    Vector2 direction = context.ReadValue<Vector2>();
+
+    if (direction.y == 0)
+    {
+      return;
+    }
+
+    StartCoroutine(ReselectFirstMenuObject());
+  }
+
+  IEnumerator ReselectFirstMenuObject()
+  {
+    yield return null;
+    if (EventSystem.current.currentSelectedGameObject == null)
+    {
+      EventSystem.current.SetSelectedGameObject(firstMenuObject);
+    }
   }
 
   public void PlayGame()
